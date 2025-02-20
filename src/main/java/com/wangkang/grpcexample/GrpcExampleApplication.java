dd530154f32947ce77ce6b695940b6e853040b9f
10004639 <wangkang@shein.com>
2025-02-20 09:41:22 +0800

    first commit

---
+++ src/main/java/com/wangkang/grpcexample/GrpcExampleApplication.java
@@ -0,0 +1,13 @@
+package com.wangkang.grpcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangkang/grpcexample/GrpcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangkang.grpcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
